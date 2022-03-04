e019e1c90803e6d108a05a753f216ca13ccd400a
Nathanael Gray <ngray@forefrontweb.com>
2022-03-04 10:03:08 -0500

    holy hell

--- gatsby-config.js
+++ gatsby-config.js
@@ -17,7 +17,7 @@ module.exports = {
         resolve: `gatsby-source-filesystem`,
         options: {
           name: `assets`,
-          path: `${__dirname}/images`,
+          path: `${__dirname}/content/assets`,
         },
       },
     ]
